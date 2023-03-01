0721c8a3b575c7009082f4719f4af337c1c89b6a
Khodja Moussa <khodja.moussa.pro@gmail.com>
2023-03-01 14:21:42 +0100

    Add named colors

---
+++ src/themes/colors.ts
@@ -0,0 +1,34 @@
+export const colors = {
+    primary: {
+        default: "#7476ED",
+        disabled: "#C4C5F1",
+    },
+    secondary: {
+        default: "#3A3B7B",
+        disabled: "#9090AB",
+    },
+    neutral: {
+        white: "white",
+        "10" : "#EAEAEC",
+        "20" : "#D6D5D9",
+        "30" : "#C1C0C7",
+        "40" : "#A5A5AA",
+        "50" : "#9897A1",
+        "60" : "#83828E",
+        "70" : "#6F6D7B",
+        "80" : "#5A5869",
+        "90" : "#464356",
+        "100" : "#312E43",
+    },
+    states: {
+        
+    },
+    background: {
+        default: "#F8F4FF"
+    },
+    services: {
+        rose: "#BF7EE6",
+        aqua: "#64C2DB",
+        pagopa: "#0066CC"
+    }
+}
